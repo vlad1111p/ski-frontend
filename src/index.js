84c12c3cb6906c2ad9e47d931bb149cf27e61c39
vlad1111p <vlad1111p@gmail.com>
2025-06-22 23:33:37 +0300

    Enable logging

--- src/index.js
+++ src/index.js
@@ -1,34 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "assets/styles/tailwind.css";
 
-// layouts
-
+// Layouts
 import Admin from "layouts/Admin.js";
 import Auth from "layouts/Auth.js";
 
-// views without layouts
-
+// Views
 import Landing from "views/Landing.js";
 import Profile from "views/Profile.js";
 import Index from "views/Index.js";
 
+//TODO save user authentication state in localStorage or context
+const isAuthenticated = () => {
+    return false;
+};
+
+const PrivateRoute = ({component: Component, ...rest}) => (
+    <Route
+        {...rest}
+        render={props =>
+            isAuthenticated() ? (
+                <Component {...props} />
+            ) : (
+                <Redirect to="/auth"/>
+            )
+        }
+    />
+);
+
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      {/* add routes with layouts */}
-      <Route path="/admin" component={Admin} />
-      <Route path="/auth" component={Auth} />
-      {/* add routes without layouts */}
-      <Route path="/landing" exact component={Landing} />
-      <Route path="/profile" exact component={Profile} />
-      <Route path="/" exact component={Index} />
-      {/* add redirect for first page */}
-      <Redirect from="*" to="/" />
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById("root")
+    <BrowserRouter>
+        <Switch>
+            {/* Protected routes */}
+            <PrivateRoute path="/admin" component={Admin}/>
+            <Route path="/auth" component={Auth}/>
+            <Route path="/landing" exact component={Landing}/>
+            <PrivateRoute path="/profile" exact component={Profile}/>
+            <Route path="/" exact component={Index}/>
+            <Redirect from="*" to="/"/>
+        </Switch>
+    </BrowserRouter>,
+    document.getElementById("root")
 );
